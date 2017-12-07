ca98aff7baa2e5c58476cef1c3e106d4e6e1bf02
Stefan Koshy <noreply@skoshy.com>
2017-12-07 14:40:45 -0500

    Changes db structure again, adds basic logging to DB, adds momentjs

--- view.js
+++ view.js
@@ -5,6 +5,8 @@ const config = require('./config.json');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./db.sqlite3');
 
+var moment = require('moment');
+
 const key = config.sync.gdax.key;
 const b64secret = config.sync.gdax.b64secret;
 const passphrase = config.sync.gdax.passphrase;
@@ -27,7 +29,29 @@ authedClient.getAccounts((error, response, data) => {
 			// loop through all accounts, get their info
 			var account = data[i];
 			authedClient.getAccountHistory(account.id, (error, response, data) => {
-				console.log(data);
+				console.log(data, "===================================");
+
+				var currentTimeInUtc = moment.utc().format();
+				var dbParams = {
+					$type: "trade",
+					$fk_currency_from_id: 1,
+					$fk_currency_to_id: 2,
+					$amount_from: 100,
+					$amount_to: 100,
+					$fee_from: 100,
+					$fee_to: 100,
+					$fk_source_id: 1,
+					$datetime_executed: currentTimeInUtc,
+					$datetime_imported: currentTimeInUtc,
+					$datetime_updated: currentTimeInUtc,
+				};
+
+				db.run(`
+					INSERT INTO trades
+					(type, fk_currency_from_id, fk_currency_to_id, amount_from, amount_to, fee_from, fee_to, fk_source_id, datetime_executed, datetime_imported, datetime_updated)
+					VALUES
+					($type, $fk_currency_from_id, $fk_currency_to_id, $amount_from, $amount_to, $fee_from, $fee_to, $fk_source_id, $datetime_executed, $datetime_imported, $datetime_updated)`
+					, dbParams);
 			});
 		}
 	}
